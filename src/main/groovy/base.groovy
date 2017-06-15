c7024d8c8c4ceb03b436c9dd2efe415261fd77e6
guillepb10 <M1thr@nd1r>
2017-06-15 21:46:08 +0200

    jenkinsfile

--- src/main/groovy/base.groovy
+++ src/main/groovy/base.groovy
@@ -2,30 +2,22 @@
 
 def run() {
     stage('Checkout') {
-        steps {
-            sh 'git clone https://github.com/guillepb10/confi-server.git'
-        }
+        sh 'git clone https://github.com/guillepb10/confi-server.git'
     }
 
     stage('Build') {
-        steps {
-            dir('confi-server') {
-                sh 'mvn clean install'
-            }
+        dir('confi-server') {
+            sh 'mvn clean install'
         }
     }
     stage('Package') {
-        steps {
-            dir('confi-server') {
-                sh '$(aws ecr get-login --region eu-west-1) && mvn package docker:build -pl docker'
-            }
+        dir('confi-server') {
+            sh '$(aws ecr get-login --region eu-west-1) && mvn package docker:build -pl docker'
         }
     }
     stage('Deploy') {
-        steps {
-            dir('confi-server') {
-                sh '$(aws ecr get-login --region eu-west-1) &&  mvn package docker:push -pl docker'
-            }
+        dir('confi-server') {
+            sh '$(aws ecr get-login --region eu-west-1) &&  mvn package docker:push -pl docker'
         }
     }
 }
